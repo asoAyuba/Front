febdd18c9ca932046fe938b9e1565296c24d82c1
asoAyuba <asociacionAyuba@gmail.com>
2024-11-24 15:47:18 +0100

    add backend request for /cats/adoptables/

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,83 +1,37 @@
-// Controlador para la página principal
-exports.home = (req, res) => {
-  const gatos = [
-      {
-          id: '1',
-          name: 'Oliver',
-          image: '/images/oliver.jpg',
-          description: 'Un gato cariñoso y juguetón que busca un hogar amoroso.',
-          gender: 'Macho',
-          observations: 'Le encanta jugar con otros gatos.'
-      },
-      {
-          id: '2',
-          name: 'Luna',
-          image: '/images/luna.jpg',
-          description: 'Una gata tranquila y dulce, ideal para un hogar calmado.',
-          gender: 'Hembra',
-          observations: 'Tiene un pelaje muy suave y le gusta dormir mucho.'
-      },
-      {
-          id: '3',
-          name: 'Max',
-          image: '/images/max.jpg',
-          description: 'Max es un gato curioso que siempre está explorando.',
-          gender: 'Macho',
-          observations: 'Tiene un pequeño problema en una pata, pero es muy activo.'
-      },
-      {
-          id: '5',
-          name: 'Mia',
-          image: '/images/mia.jpg',
-          description: 'Mia es una gata muy cariñosa que ama ser acariciada.',
-          gender: 'Hembra',
-          observations: 'Prefiere estar sola o con humanos, no le gustan mucho otros gatos.'
-      },
-      {
-        id: '6',
-        name: 'Oliver',
-        image: '/images/oliver.jpg',
-        description: 'Un gato cariñoso y juguetón que busca un hogar amoroso.',
-        gender: 'Macho',
-        observations: 'Le encanta jugar con otros gatos.'
-    },
-    {
-        id: '7',
-        name: 'Luna',
-        image: '/images/luna.jpg',
-        description: 'Una gata tranquila y dulce, ideal para un hogar calmado.',
-        gender: 'Hembra',
-        observations: 'Tiene un pelaje muy suave y le gusta dormir mucho.'
-    },
-    {
-        id: '8',
-        name: 'Max',
-        image: '/images/max.jpg',
-        description: 'Max es un gato curioso que siempre está explorando.',
-        gender: 'Macho',
-        observations: 'Tiene un pequeño problema en una pata, pero es muy activo.'
-    },
-    {
-        id: '9',
-        name: 'Mia',
-        image: '/images/mia.jpg',
-        description: 'Mia es una gata muy cariñosa que ama ser acariciada.',
-        gender: 'Hembra',
-        observations: 'Prefiere estar sola o con humanos, no le gustan mucho otros gatos.'
-    }
-  ];
+const axios = require('axios'); // Para realizar solicitudes HTTP
 
-  // Renderizar la vista 'home' pasando todas las variables
-  res.render('home', {
-      title: 'Página Principal',
-      message: 'Bienvenido a la protectora de gatos',
-      gatos
-  });
-}
+// Controlador para la página principal
+exports.home = async (req, res) => {
+    try {
+      // Realizar la llamada al endpoint del back-end
+      const response = await axios.get('http://localhost:4000/cats/adoptables');
+      const gatos = response.data;
+  
+      console.log('Datos de gatos adoptables:', gatos);
+  
+      // Renderizar la vista 'home' con los datos obtenidos
+      res.render('home', {
+        title: 'Página Principal',
+        message: 'Bienvenido a la protectora de gatos',
+        gatos
+      });
+    } catch (error) {
+      console.error('Error al obtener los gatos adoptables:', error.message);
+  
+      // Renderizar la vista 'home' con un mensaje de error y sin gatos
+      res.render('home', {
+        title: 'Página Principal',
+        message: 'Bienvenido a la protectora de gatos',
+        gatos: [],
+        error: 'No se pudieron cargar los gatos adoptables'
+      });
+    }
+  };
+  
   // Controlador para la página "about"
   exports.about = (req, res) => {
-    res.render('about', { title: 'Sobre Nosotros', message: 'Información sobre la protectora' });
+    res.render('about', {
+      title: 'Sobre Nosotros',
+      message: 'Información sobre la protectora'
+    });
   };
-
-
-
