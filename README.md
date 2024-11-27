d86da5022de16510a0eecea3b8f18f30086720fc
asoAyuba <asociacionAyuba@gmail.com>
2024-11-27 19:17:33 +0100

    add changes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Ayuba-Web
-# Front
+# Front-End
+
+docker-compose down
+docker-compose up --build
+docker network inspect app-network
+
+
+docker network create app-network
+
+docker-compose up --no-recreate
+
+docker network connect app-network <backend-container-id>
+docker network connect app-network <frontend-container-id>
+
+
+
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,31 +2,28 @@ const axios = require('axios'); // Para realizar solicitudes HTTP
 
 // Controlador para la página principal
 exports.home = async (req, res) => {
-    try {
-      // Realizar la llamada al endpoint del back-end
-      const response = await axios.get('http://localhost:4000/cats/adoptables');
-      const gatos = response.data;
-  
-      console.log('Datos de gatos adoptables:', gatos);
-  
-      // Renderizar la vista 'home' con los datos obtenidos
-      res.render('home', {
-        title: 'Página Principal',
-        message: 'Bienvenido a la protectora de gatos',
-        gatos
-      });
-    } catch (error) {
-      console.error('Error al obtener los gatos adoptables:', error.message);
-  
-      // Renderizar la vista 'home' con un mensaje de error y sin gatos
-      res.render('home', {
-        title: 'Página Principal',
-        message: 'Bienvenido a la protectora de gatos',
-        gatos: [],
-        error: 'No se pudieron cargar los gatos adoptables'
-      });
-    }
-  };
+  try {
+    // Usa el nombre del servicio del backend
+    const response = await axios.get('http://localhost:4000/cats/adoptables');
+    const gatos = response.data;
+
+    res.render('home', {
+      title: 'Página Principal',
+      message: 'Bienvenido a la protectora de gatos',
+      gatos,
+      error: null,
+    });
+  } catch (err) {
+    console.error('Error al obtener los gatos adoptables:', err.message);
+
+    res.render('home', {
+      title: 'Página Principal',
+      message: 'Bienvenido a la protectora de gatos',
+      gatos: [],
+      error: 'No se pudieron cargar los gatos adoptables',
+    });
+  }
+};
   
   // Controlador para la página "about"
   exports.about = (req, res) => {
